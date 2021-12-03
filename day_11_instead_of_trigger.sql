6767f823e0615db251ed6b4663c94f1603b58836
Bùi Hữu Lộc <91431461+maoleng@users.noreply.github.com>
2021-12-03 16:40:23 +0700

    03-12-20201

---
+++ day_11_instead_of_trigger.sql
@@ -0,0 +1,191 @@
+﻿drop database sinh_vien
+create database sinh_vien
+use sinh_vien
+
+drop table lop
+create table lop(
+ma_lop int,
+ten_lop nvarchar(50),
+so_cho_trong int,
+primary key (ma_lop)
+)
+
+drop table sinh_vien
+create table sinh_vien(
+ma_sinh_vien int ,
+ten_sinh_vien nvarchar(50),
+ma_lop int,
+primary key (ma_sinh_vien),
+foreign key (ma_lop) references lop(ma_lop)
+)
+
+insert into lop(ma_lop, ten_lop, so_cho_trong)
+values (1, 'KHMT', 5), (2, 'KTPM', 5), (3, 'MMT', 5)
+
+
+--trigger instead of them sinh vien
+drop trigger trigger_them_sinh_vien
+create trigger trigger_them_sinh_vien
+on sinh_vien
+instead of insert
+as
+begin
+	declare @ma_lop int = (select ma_lop from inserted)	
+	declare @so_cho_trong int = (select so_cho_trong from lop where ma_lop = @ma_lop)
+	if (@so_cho_trong = 0)
+		begin
+			print(N'Hết chỗ')
+		end
+	else
+		begin
+			update lop set
+			so_cho_trong -= 1
+			where ma_lop = @ma_lop
+			declare @ma_sinh_vien int = (select ma_sinh_vien from inserted)
+			declare @ten_sinh_vien nvarchar(50) = (select ten_sinh_vien from inserted)
+			insert into sinh_vien(ma_sinh_vien, ten_sinh_vien, ma_lop)
+			values(@ma_sinh_vien, @ten_sinh_vien, @ma_lop)
+			print(N'Đã thêm sinh viên')
+		end
+
+
+end
+
+insert into sinh_vien
+values (2, 'Khoa', 1)
+
+
+--trigger instead of chuyen lop cho sinh vien
+drop trigger trigger_chuyen_sinh_vien
+create trigger trigger_chuyen_sinh_vien
+on sinh_vien
+instead of update
+as 
+begin
+	--lay ra ma lop ban sinh vien vua insert/delete, kiem tra so cho trong la bao nhieu
+	--neu so cho trong = 0 thi out chuong trinh, khac 0 thi tiep tuc
+	--tru 1 di so cho trong where malop = @malopcu 
+	--+ 1 so cho trong where malop = @malopmoi
+	declare @ma_lop_cu int = (select ma_lop from deleted)
+	declare @ma_lop_moi int = (select ma_lop from inserted)
+	declare @so_cho_trong_lop_cu int = (select so_cho_trong from lop where ma_lop = @ma_lop_cu)
+	declare @so_cho_trong_lop_moi int = (select so_cho_trong from lop where ma_lop = @ma_lop_moi)
+	
+	if (@so_cho_trong_lop_moi = 0)
+		begin
+			print(N'Đã hết chỗ')
+		end
+	else
+		begin
+			update lop set
+			so_cho_trong = so_cho_trong + 1
+			where ma_lop = @ma_lop_cu
+			
+			update lop set
+			so_cho_trong -= 1
+			where ma_lop = @ma_lop_moi
+
+
+			--ko nen lam nhu vay, vi nhu vay chi cap nhat duoc moi viec chuyen lop
+			--declare @ma_sinh_vien int = (select ma_sinh_vien from inserted where ma_lop = @ma_lop_moi)
+			--update sinh_vien set
+			--ma_lop = @ma_lop_moi
+			--where ma_sinh_vien = @ma_sinh_vien
+
+			--nen lam nhu vay
+			delete sinh_vien where ma_lop = @ma_lop_cu
+			
+			--declare @ma_sinh_vien int = (select ma_sinh_vien from inserted where ma_lop = @ma_lop_moi)
+			--declare @ten_sinh_vien nvarchar(50) = (select ten_sinh_vien from inserted where ma_lop = @ma_lop_moi)
+			--insert into sinh_vien(ma_sinh_vien, ten_sinh_vien, ma_lop)
+			--values (@ma_sinh_vien, @ten_sinh_vien, @ma_lop_moi)
+			insert into sinh_vien
+			select * from inserted
+		end
+		
+		select * from sinh_vien
+		select * from lop
+end
+
+update sinh_vien
+set ma_lop = 1 where ma_sinh_vien = 2
+
+--trigger them 1 luc nhieu sinh vien
+drop trigger trigger_them_nhieu_sinh_vien
+create or alter trigger trigger_them_nhieu_sinh_vien
+on sinh_vien
+instead of insert
+as
+begin
+	--dem so luong sinh vien them vao chia ra theo tung lop & dem so cho trong theo tung lop
+	--kiem tra so luong sinh vien them vao co be hon so cho trong khong ? tra ve nhung malop hop le
+	--insert nhung ban hop le vao 
+	--cap nhat bang lop 
+	insert into sinh_vien
+	select * from inserted
+	where ma_lop in (
+		select lop.ma_lop from lop
+		join
+		(select ma_lop, count(*) as 'So_luong_sinh_vien_se_them_vao' from inserted group by ma_lop)  as i
+		on lop.ma_lop  = i.ma_lop
+		where (So_luong_sinh_vien_se_them_vao <= so_cho_trong)
+	)
+	update lop
+	set so_cho_trong -= i.So_luong_sinh_vien_se_them_vao
+	from 
+		lop
+		join
+		(select ma_lop, count(*) as 'So_luong_sinh_vien_se_them_vao' from inserted group by ma_lop)  as i
+		on lop.ma_lop  = i.ma_lop
+		where (So_luong_sinh_vien_se_them_vao <= so_cho_trong)
+	
+end
+
+insert into sinh_vien
+values (5, 'Long', 3), (6, 'Ken', 2), (7, 'Ken', 2), (8, 'Ken', 1)
+
+
+
+create or alter trigger trigger_cap_nhat_1_luc_nhieu_sinh_vien
+on sinh_vien
+instead of update
+as
+begin
+	--đếm số bạn chuyển, so sánh với số chỗ trống -> lấy ra những bạn có thể chuyển	
+	--cột mã không tự động tăng mới làm được
+	--select lọc ra 1 số thông tin , insert vào bảng sinh viên những thông tin đó
+	delete sinh_vien from sinh_vien
+	join deleted on deleted.ma_sinh_vien = sinh_vien.ma_sinh_vien
+	join inserted on inserted.ma_sinh_vien = inserted.ma_sinh_vien
+	where inserted.ma_lop in(
+		select lop.ma_lop from lop join
+			(select ma_lop, count(*) as 'so_ban_duoc_chuyen' from inserted group by ma_lop) as i
+			on lop.ma_lop = i.ma_lop
+			where i.so_ban_duoc_chuyen <= lop.so_cho_trong
+	)
+	
+	insert into sinh_vien
+	select * from inserted
+	where ma_lop in(
+		select lop.ma_lop from lop join
+		(select ma_lop, count(*) as 'so_ban_duoc_chuyen' from inserted group by ma_lop) as i
+		on lop.ma_lop = i.ma_lop
+		where i.so_ban_duoc_chuyen <= lop.so_cho_trong
+	)
+
+
+	select * from sinh_vien
+	select * from lop
+
+end
+
+
+
+delete from sinh_vien
+
+update sinh_vien set
+ma_lop = 3
+where ma_sinh_vien in (2,3)
+
+select * from sinh_vien
+select * from lop
---
+++ day_11_trigger_homework.sql
@@ -0,0 +1,137 @@
+﻿
+(*: Kiểm tra sức chứa tương ứng với tổng số lượng sản phẩm, nếu còn dư mới được cập nhật vào kho đó)
+
+drop database quan_ly_san_pham
+create database quan_ly_san_pham
+use quan_ly_san_pham
+
+
+--Kho (mã, tên, tổng tài sản, sức chứa)
+drop table kho
+create table kho(
+ma_kho int,
+ten_kho nvarchar(50),
+tong_tai_san int default 0,
+suc_chua int default 50,
+primary key (ma_kho)
+)
+
+insert into kho(ma_kho, ten_kho)
+values (1, 'Keo'), (2, 'Bong bay')
+
+--Sản phẩm (mã, tên, số lượng, giá trị, mã kho)
+drop table san_pham
+create table san_pham(
+ma_san_pham int ,
+ten_san_pham nvarchar (50),
+so_luong int,
+gia_tri int,
+ma_kho int,
+primary key(ma_san_pham),
+foreign key (ma_kho) references kho(ma_kho)
+)
+
+--Tạo procedure để thêm và hiển thị lại giá trị vừa thêm ở từng bảng
+create procedure them_va_hien_thi_gia_tri_vua_them_san_pham
+@ten_san_pham nvarchar(50),
+@so_luong int,
+@gia_tri int,
+@ma_kho int
+as
+begin
+	insert into san_pham(ten_san_pham, so_luong, gia_tri, ma_kho)
+	values (@ten_san_pham, @so_luong, @gia_tri, @ma_kho)
+	select top 1* from san_pham order by ma_san_pham desc
+
+end
+--Kho (mã, tên, tổng tài sản, sức chứa)
+drop proc them_va_hien_thi_gia_tri_vua_them_kho
+create procedure them_va_hien_thi_gia_tri_vua_them_kho
+@ten_kho nvarchar(50),
+@suc_chua int
+as
+begin
+	insert into kho(ten_kho, suc_chua)
+	values (@ten_kho, @suc_chua)
+	select top 1* from kho order by ma_kho desc
+end
+
+them_va_hien_thi_gia_tri_vua_them_kho @ten_kho = N'Kho kẹo', @suc_chua = 20
+them_va_hien_thi_gia_tri_vua_them_san_pham @ten_san_pham = N'Kẹo nổ'  , @gia_tri = 20, @so_luong = 10, @ma_kho = 2
+
+
+--Tạo trigger phù hợp để Khi cập nhật sản phẩm thì cập nhật tổng tài sản tương ứng với kho
+drop trigger cap_nhat_tong_tai_san_tuong_ung_kho
+create or alter trigger cap_nhat_tong_tai_san_tuong_ung_kho
+on san_pham
+after insert, delete, update
+as
+begin
+	--update 
+	update kho set
+	tong_tai_san = tong_tai_san + tong_tien_cua_san_pham
+	from kho join
+	--Lấy ra tổng tài sản của 1 sản phẩm
+	(select ma_kho, sum(gia_tri * so_luong) as 'tong_tien_cua_san_pham' from inserted group by ma_kho ) as i
+	on i.ma_kho = kho.ma_kho
+
+	update kho set
+	tong_tai_san = tong_tai_san - tong_tien_cua_san_pham
+	from kho join
+	--Lấy ra tổng tài sản của 1 sản phẩm
+	(select ma_kho, sum(gia_tri * so_luong) as 'tong_tien_cua_san_pham' from deleted group by ma_kho ) as d
+	on d.ma_kho = kho.ma_kho
+
+	select * from kho
+	select * from san_pham
+
+end
+
+insert into san_pham(ma_san_pham, ten_san_pham, so_luong, gia_tri, ma_kho)
+values (1, 'keo dong', 5,10,1),(2, 'bong dong', 5,10,2)
+
+--Khi xoá kho thì kiểm tra sản phẩm có tồn tại không, nếu không mới được xoá
+create or alter trigger kiem_tra_kho_truoc_khi_xoa
+on kho
+instead of delete
+as
+begin
+	delete from kho
+	where ma_kho in (
+		--xóa những kho người dùng yêu cầu
+		select ma_kho from deleted
+		--chỉ xóa những kho k có sản phẩm
+		where ma_kho not in(
+			select ma_san_pham from san_pham
+		)
+	)
+
+	select ma_kho, 'Khong the xoa do co san pham' as 'Thong bao' from kho
+	where ma_kho in (
+		select ma_kho from deleted
+		where ma_kho in (
+			select ma_san_pham from san_pham
+		)
+	)
+
+
+end
+
+
+
+
+delete from san_pham
+where ma_san_pham in (3,4)
+
+update san_pham set ma_kho = 1 where ma_san_pham  = 2
+
+select * from kho
+select * from san_pham
+
+delete from kho
+where ma_kho = 1
+
+
+insert into kho
+values (3, 'A', '0', 50)
+
